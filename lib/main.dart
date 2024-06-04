56cf6301e9ea6f01f8f8689d64153ab4adca341a
iramadani2003@gmail.com <iramadani2003@gmail.com>
2024-06-04 14:06:43 +0700

    event tracaker

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:time_minder/services/onboarding_routes.dart';
+import 'package:time_minder/services/tracker_service.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:time_minder/widgets/common/providers.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,14 +15,19 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MultiProvider(
+  runApp(
+    MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => SelectedItemsProvider()),
       ],
       child: const MainApp(),
-    ),);
+    ),
+  );
   requestNotificationPermission();
   await ScreenUtil.ensureScreenSize();
+  await dotenv.load(fileName: ".env");
+  await (TrackerService()).track("on-open-app", withDeviceInfo: true);
+  await (TrackerService()).track("on-load-app", withDeviceInfo: true);
 }
 
 void requestNotificationPermission() async {
@@ -31,7 +38,7 @@ void requestNotificationPermission() async {
 }
 
 class MainApp extends StatefulWidget {
-   const MainApp({super.key});
+  const MainApp({super.key});
 
   @override
   State<MainApp> createState() => _MainAppState();
